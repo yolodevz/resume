19fbb080bd686f81c79ac9daac18fb7aaa57d2a1
michal-wasilewski <michal@thefabricant.com>
2023-07-30 15:31:12 +0100

    chore(git ignore): ignore storybook-static

--- apps/workshop/storybook-static/assets/Text.stories-5633e1e6.js
+++
@@ -1,6 +0,0 @@
-import{T as a}from"./index-fd015fce.js";import"./index-751f020d.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";const p={title:"yolo/components/Text",component:a,parameters:{layout:"centered"},tags:["autodocs"]},e={args:{children:"change me"}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    children: "change me"
-  }
-}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const d=["Default"];export{e as Default,d as __namedExportsOrder,p as default};
-//# sourceMappingURL=Text.stories-5633e1e6.js.map
