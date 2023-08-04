c5e34e475ea38d11e7700c96c421ce2fc09a360f
michal-wasilewski <michal@thefabricant.com>
2023-08-05 00:55:31 +0100

    perf: check with intersection observer

--- packages/monogram-ui/src/components/animated/3d/Sphere.tsx
+++ packages/monogram-ui/src/components/animated/3d/Sphere.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-// Don't include THREE in the bundle and use @react-three instead if possible.
 import React, { useRef } from "react";
-import { Canvas, useFrame, type MeshProps } from "@react-three/fiber";
+import { useInView } from "react-intersection-observer";
+import { Canvas, useFrame } from "@react-three/fiber";
 import {
   MeshDistortMaterial,
   GradientTexture,
@@ -14,12 +14,9 @@ import {
 function SphereObject() {
   const meshRef = useRef<any>(null);
 
-  // use useFrame hook to get performant animations (instead of doing it with react hooks)
   useFrame((state) => {
     if (meshRef.current) {
-      const material = meshRef.current.material as MeshProps & {
-        distort?: number;
-      };
+      const material = meshRef.current.material as any;
       const elapsedTime = state.clock.getElapsedTime();
       material.distort = (1 + Math.sin(elapsedTime * 0.5)) * 0.15;
     }
@@ -29,8 +26,8 @@ function SphereObject() {
     <Center>
       <mesh
         ref={meshRef}
-        onPointerOver={() => ((meshRef.current?.material as any).distort = 0.3)} // manipulate the object directly to avoid additional state management
-        onPointerOut={() => ((meshRef.current?.material as any).distort = 0)}
+        onPointerOver={() => (meshRef.current.material.distort = 0.3)}
+        onPointerOut={() => (meshRef.current.material.distort = 0)}
       >
         <sphereGeometry args={[0.75, 64, 64]} />
         <MeshDistortMaterial speed={3} roughness={1}>
@@ -50,19 +47,26 @@ function SphereObject() {
 }
 
 export const Sphere = () => {
+  const { ref, inView } = useInView({ threshold: 0 });
+
   return (
-    <Canvas shadows camera={{ position: [0, 0, 2], fov: 50 }}>
-      <SphereObject />
-      <OrbitControls
-        autoRotate
-        autoRotateSpeed={4}
-        enablePan={false}
-        enableZoom={false}
-        minPolarAngle={Math.PI / 2.1}
-        maxPolarAngle={Math.PI / 2.1}
-      />
-      <Environment blur={1} preset={"warehouse"} />
-    </Canvas>
+    <div ref={ref} className="h-full">
+      {inView && (
+        <Canvas shadows camera={{ position: [0, 0, 2], fov: 50 }}>
+          <SphereObject />
+          <OrbitControls
+            autoRotate
+            autoRotateSpeed={4}
+            enablePan={false}
+            enableZoom={false}
+            minZoom={2}
+            minPolarAngle={Math.PI / 2.1}
+            maxPolarAngle={Math.PI / 2.1}
+          />
+          <Environment blur={1} preset={"warehouse"} />
+        </Canvas>
+      )}
+    </div>
   );
 };
 
