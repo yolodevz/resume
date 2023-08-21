86d1d4ce25a9eaf74dce4768a9f1bacc6430d121
michal-wasilewski <michal@thefabricant.com>
2023-08-21 21:05:24 +0100

    refactor: update layout font and meta

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,15 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
 import "@yolo/ui/styles/global.css";
+
+export const metadata: Metadata = {
+  title: "YOLODEV Frontend Developer",
+  description: `Senior Creative Frontend Developer,
+CSS Connoisseur & Tech Transitioner`,
+};
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -7,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="flex min-h-screen flex-col bg-background-primary">
+      <body
+        className={`flex min-h-screen flex-col bg-background-primary ${inter.className}`}
+      >
         {children}
       </body>
     </html>
